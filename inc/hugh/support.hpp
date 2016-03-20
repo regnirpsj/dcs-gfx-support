d570684bb6e9d09e0b8ebb9b3b067f74d03ef617
jan p springer <jsd@igroup.org>
2016-03-20 12:26:15 +0000

    added: top-level header

---
+++ inc/hugh/support.hpp
@@ -0,0 +1,48 @@
+// -*- Mode:C++ -*-
+
+/**************************************************************************************************/
+/*                                                                                                */
+/* Copyright (C) 2016 University of Hull                                                          */
+/*                                                                                                */
+/**************************************************************************************************/
+/*                                                                                                */
+/*  module     :  hugh/support.hpp                                                                */
+/*  project    :                                                                                  */
+/*  description:                                                                                  */
+/*                                                                                                */
+/**************************************************************************************************/
+
+#if !defined(HUGH_SUPPORT_HPP)
+
+#define HUGH_SUPPORT_HPP
+
+// includes, system
+
+//#include <>
+
+// includes, project
+
+#include <hugh/support/chrono.hpp>
+#include <hugh/support/hasher.hpp>
+#include <hugh/support/io_utils.hpp>
+#include <hugh/support/printable.hpp>
+#include <hugh/support/refcounted.hpp>
+#include <hugh/support/signal_handler.hpp>
+#include <hugh/support/string.hpp>
+#include <hugh/support/thread.hpp>
+#include <hugh/support/trace.hpp>
+#include <hugh/support/type_info.hpp>
+
+namespace hugh {
+  
+  // types, exported (class, enum, struct, union, typedef)
+
+  // variables, exported (extern)
+
+  // functions, inlined (inline)
+  
+  // functions, exported (extern)
+  
+} // namespace hugh {
+
+#endif // #if !defined(HUGH_SUPPORT_HPP)
