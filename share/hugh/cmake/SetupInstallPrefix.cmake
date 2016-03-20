3753fd73cd7c328ac6f418e9ea70159618f06b45
jan p springer <jsd@igroup.org>
2016-03-20 10:08:25 +0000

    fixed: install-prefix detection

--- share/hugh/cmake/SetupInstallPrefix.cmake
+++ share/hugh/cmake/SetupInstallPrefix.cmake
@@ -6,10 +6,14 @@
 #                                                                                                  #
 ####################################################################################################
 
-if(NOT CMAKE_BUILD_TYPE)
-  set(CMAKE_BUILD_TYPE "RelWithDebInfo")
+if(NOT CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "${CMAKE_SOURCE_DIR}/install")
+
+  if(DEFINED ENV{HUGH_INSTALL_PREFIX})
+    set(CMAKE_INSTALL_PREFIX $ENV{HUGH_INSTALL_PREFIX})
+  endif()
 endif()
 
 if(VERBOSE)
-  message(STATUS "CMAKE_BUILD_TYPE set to '" ${CMAKE_BUILD_TYPE} "'")
+  message(STATUS "CMAKE_INSTALL_PREFIX set to '" ${CMAKE_INSTALL_PREFIX} "'")
 endif()
