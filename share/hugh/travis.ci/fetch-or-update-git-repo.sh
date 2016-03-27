a885193ea128befbdd6a9e819aa2ef1beebfd9ed
jan p springer <jsd@igroup.org>
2016-03-27 16:11:26 +0100

    fixed: variable setup

--- share/hugh/travis.ci/fetch-or-update-git-repo.sh
+++ share/hugh/travis.ci/fetch-or-update-git-repo.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 set -e
 
-args=("$@")
-
-# ${args[0]}: repository (e.g., http://github.com/g-truc/gli.git)
-# ${args[1]}: directory  (e.g., ${HOME}/extra/gli-git)
-# ${args[2]}: branch     (e.g., 0.8.1)
+# $1: repository (e.g., http://github.com/g-truc/gli.git)
+# $2: directory  (e.g., ${HOME}/extra/gli-git)
+# $3: branch     (e.g., 0.8.1)
 
 if [ 2 -gt "$#" ]; then
     echo "need at least two args!"
     exit 1
 fi
 
-if [ 4 -gt "$#" ]; then
-    branch="${args[2]}"
-else
-    branch="master"
+branch="master"
+
+if [ 4 -lt "$#" ]; then
+    branch="$3"
 fi
 
-if [ ! -d ${args[1]} ]; then
-    echo "creating '${args[1]}'"
-    mkdir -p ${args[1]}
+if [ ! -d $2 -o ! -d "$2/.git" ]; then
+    echo "creating '$2'"
+    mkdir -p $2
 
-    echo "cloning '${args[0]} ${branch}' -> '${args[1]}'"
-    git clone -b ${branch} ${args[0]} ${args[1]}
+    echo "cloning '$1 $branch' -> '$2'"
+    git clone -b $branch $1 $2
 else
-    echo "updating '${args[1]}'"
-    cd ${args[1]}
+    echo "updating '$2'"
+    cd $2
     git pull
 fi
