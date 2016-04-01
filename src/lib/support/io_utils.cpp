dc8d4a7146f2c5f220307655fb4064a522d53255
jan p springer <jsd@igroup.org>
2016-04-01 09:34:03 +0100

    update: extended binary printer for un/formatted output

--- src/lib/support/io_utils.cpp
+++ src/lib/support/io_utils.cpp
@@ -63,30 +63,35 @@ namespace hugh {
       /* virtual */ void
       binary::print_on(std::ostream& os) const
       {
-#if 0
-        os << '[' << std::bitset<32>(value_) << ']';
-#else
-        static std::array<std::pair<unsigned const, unsigned const>, 8> const blocks = {
-          {
-            std::make_pair(0xf0000000, 28),
-            std::make_pair(0x0f000000, 24),
-            std::make_pair(0x00f00000, 20),
-            std::make_pair(0x000f0000, 16),
-            std::make_pair(0x0000f000, 12),
-            std::make_pair(0x00000f00,  8),
-            std::make_pair(0x000000f0,  4),
-            std::make_pair(0x0000000f,  0),
+        using char_type = std::ostream::char_type;
+        
+        format_punct<char_type> const& fmt(get_facet<format_punct<char_type>>(os));
+
+        if (fmt.formatted) {
+          static std::array<std::pair<unsigned const, unsigned const>, 8> const blocks = {
+            {
+              std::make_pair(0xf0000000, 28),
+              std::make_pair(0x0f000000, 24),
+              std::make_pair(0x00f00000, 20),
+              std::make_pair(0x000f0000, 16),
+              std::make_pair(0x0000f000, 12),
+              std::make_pair(0x00000f00,  8),
+              std::make_pair(0x000000f0,  4),
+              std::make_pair(0x0000000f,  0),
+            }
+          };
+      
+          os << fmt.delim_left;
+      
+          for (auto b : blocks) {
+            os << std::bitset<4>((value_ & b.first) >> b.second) << fmt.separator;
           }
-        };
       
-        os << '[';
-      
-        for (auto b : blocks) {
-          os << std::bitset<4>((value_ & b.first) >> b.second) << '.';
+          os << remove(1)
+             << fmt.delim_right;
+        } else {
+          os << std::bitset<32>(value_);
         }
-      
-        os << "\b]";
-#endif
       }
     
     } // namespace ostream  {
